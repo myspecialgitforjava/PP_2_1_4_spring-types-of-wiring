0a46b3cf1d4ff17b41e266f361cdea8409b06ec5
Кирилл Крайнов <117298021+myspecialgitforjava@users.noreply.github.com>
2023-02-24 13:31:05 +0300

    Add files via upload

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,15 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+
+    @Override
+    public String toString() {
+        return "смерть Кощея";
+    }
+
+
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private final Deth8 death;
+
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", " + death.toString() + " на игле :( ";
     }
 }
